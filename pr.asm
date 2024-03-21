c8b063ed6ca777f48554f449e79020c18c17ee28
MaksymSemk <maksumsemen2006@gmail.com>
2024-03-21 23:00:29 +0200

    resolved prosblem with multiple keys

--- pr.asm
+++ pr.asm
@@ -2,7 +2,7 @@
 .stack 100h
 
 .data
-filename    db "in.txt", 0
+filename  db "in.txt", 0
 mes db "Succ $"
 mesBad db "File error $"
 handle dw 0
@@ -10,8 +10,12 @@ handle dw 0
 buffInd db 0; Index to keep track of the current position in buffer
 oneChar db 0
 
+presInd dw 0
+newInd dw 0
 keys db 10000*16 dup(0)
 keyInd dw 0
+keyTemp db 16 dup(0)
+keyTempInd dw 0
 isWord db 1
 values dw 10000*8 dup(0)
 valInd dw 0
@@ -75,14 +79,14 @@ pop ax
     or ax,ax
     jnz read_next
 ;clean number last number and write last number
-        mov si, offset number
-        dec numberInd
-       mov bx, numberInd
-    
-        add si, bx
-        mov al, 0
-        mov [si], al
-        call trnInNum
+     ;   mov si, offset number
+     ;   dec numberInd
+    ;   mov bx, numberInd
+;
+    ;    add si, bx
+    ;    mov al, 0
+    ;    mov [si], al
+    ;    call trnInNum
 ;fill keys array
 
  mov ah, 09h
@@ -114,21 +118,21 @@ jnz notSpace
 ;chance isWord to 0
 mov isWord,0
 ; go to next position in keys
- mov ax, keyInd
- mov cx, 16
+ ;mov ax, keyInd
+ ;mov cx, 16
  ;div cx; now ax has number of keys
- shr ax, 4        ; Shift right by 4 position
- inc ax; go to next key 
-mul cx; ax has position of next key
-mov keyInd,ax
- 
+ ;shr ax, 4        ; Shift right by 4 position
+ ;inc ax; go to next key 
+;mul cx; ax has position of next key
+;mov keyInd,ax
+ ;check if key exists
+    call checkKey
     jmp endProc
 notSpace:
     cmp isWord, 0
     jnz itsWord
        ;save char to values
        mov si, offset number
-       
         mov bx, numberInd
         add si, bx
         mov al, oneChar
@@ -138,14 +142,19 @@ notSpace:
 itsWord:
     
          ;save char to keys
-        mov si, offset keys
-
-        mov bx, keyInd 
+        ;mov si, offset keys
+       ; mov bx, keyInd 
+       ; add si, bx
+       ; mov al, oneChar
+       ; mov [si], al
+       ; inc keyInd 
+        ;add char to KeyTemp
+        mov si, offset keyTemp
+        mov bx, keyTempInd 
         add si, bx
         mov al, oneChar
-
         mov [si], al
-        inc keyInd 
+        inc keyTempInd 
       
 
 endProc:
@@ -163,6 +172,7 @@ trnInNum PROC
     
     ;add si, numberInd; last char of this number
     ;dec si
+    
     xor bx,bx
     mov cx,0
 calcNum:
@@ -207,14 +217,17 @@ afterCalc:
 ;save number into values array
 
   mov si, offset values
-  add si, valInd
- 
+  mov ax, presInd
+  shl ax, 1  ; calc real index in values
+  add si, ax
+  add bx, [si];add previously saved number
   mov [si],bx;save number into array
   ;increment valInd by 2
-  inc valInd
-  inc valInd
+  ;inc valInd
+  ;inc valInd
   mov numberInd,0
   mov cx,0
+  ;fill number by 0
   fillZeros:
     mov si, offset number
     add si, cx
@@ -223,9 +236,89 @@ afterCalc:
     cmp cx,9
     jnz fillZeros
 
-
 ret
 trnInNum endp
+
+checkKey proc
+    mov ax,0
+    mov bx, 0; presence of key
+    mov cx, 0
+    mov dx,0
+    ;check if keyInd is 0
+    cmp newInd,0
+    jnz findKey
+jmp addNewKey  
+    findKey:
+    mov dx,0
+        checkPresKey:
+        mov si, offset keys
+        shl cx, 4
+        add si, cx
+        shr cx,4
+        add si, dx; next char offset
+        mov al,[si]; next char
+        mov di, offset keyTemp
+        add di,dx
+        mov ah, [di]; next char in keyTemp
+        cmp al,ah
+        jne notEqualChar
+            mov bx,1; this char present in current key
+            jmp contComp
+            notEqualChar:
+            mov bx,0; this char dont present in current key
+            mov dx, 15; go to next key
+        contComp:
+            inc dx
+            cmp dx,16
+            jnz checkPresKey
+        ;check if key is present   
+    cmp bx,0
+    jnz keyPresent 
+    inc cx
+    cmp cx, newInd
+    jne findKey
+ ;   new key
+    ;add new key to key array
+    mov cx, 0  ; counter
+    addNewKey:
+    
+    mov si, offset keyTemp   ; addr of source
+    add si, cx
+    mov di, offset keys  ; addr of dest
+    mov ax,  newInd
+    shl ax,4 
+    add di,cx
+    add di, ax ; addr of dest
+    mov al, [si]
+    mov [di], al 
+    inc cx
+    cmp cx, 16
+    jnz addNewKey
+     mov cx, newInd
+    mov presInd,cx
+    inc newInd
+   
+    ; set new 1 to array of quantities
+    jmp endOfCheck;goto end
+
+keyPresent:
+    ;key index in cx
+    ;add 1 to this index
+    mov presInd,cx
+
+endOfCheck:
+   ;fill temp key by 0
+    mov keyTempInd,0
+    mov cx,0
+  fillZeroskey:
+    mov si, offset keyTemp
+    add si, cx
+    mov [si],0
+    inc cx
+    cmp cx,15
+    jnz fillZeroskey  
+    ret
+checkKey endp
 end main
 
 
