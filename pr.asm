bbb927b76c570eed9c1385df4b814da5541a3850
MaksymSemk <maksumsemen2006@gmail.com>
2024-03-21 16:25:15 +0200

    Added code that saves values into buffer

--- pr.asm
+++ pr.asm
@@ -6,12 +6,9 @@ filename    db "in.txt", 0
 mes db "Succ $"
 mesBad db "File error $"
 handle dw 0
-keys dw 10000*8 dup(3)
-keyCount dw 0;
-keysLength equ ($ - keys)   ; Calculate the length of the array
-oneChar dw ?
-buffer db 256 dup(0)    ; Buffer to store characters read from the file
-bufferIndex dw ?        ; Index to keep track of the current position in buffer
+buffer dw 10000*8 dup(0)
+buffInd db 0; Index to keep track of the current position in buffer
+oneChar db 0
 .code
 main:
     mov ax, @data
@@ -28,11 +25,12 @@ jc error ;Call routine to handle errors
      mov ah, 09h
  mov dx, offset mesBad
 int 21h
-jmp end
+jmp ending
 cont:
 
 mov [handle] , ax ; Save file handle for later
 
+;read file and put characters into buffer
 read_next:
     mov ah, 3Fh
     mov bx, [handle]  ; file handle
@@ -41,39 +39,36 @@ read_next:
     int 21h   ;  ax = number of bytes read
     ; do something with [oneChar]
  
+    ;save ax
+    push ax
+    ;where to write oneChar
+    mov al, oneChar        ; Load the character into AL register
+    mov bl, buffInd           ; Index of the 5th element (0-based index)
+    
+    mov si, offset buffer  ; Load the base address of the array
+    add si, bx            ; Calculate the address of the element to write into
+    
+    mov [si], al        ; Write the character into the array element
 
-    mov cx, 1  ; counter
-    mov si, offset oneChar   ; addr of source
-    mov di, offset keys  ; addr of dest
-    cld   ;  left-to-right order
-    rep movsb   ; repeat bytes movs until CX=0
+    ;add one to keyCount(pointer)
+    inc buffInd
+    inc buffInd
 
+;return ax vaulue
+pop ax
     or ax,ax
     jnz read_next
 
-;print array
 
-; Loop through the array and print each element
- ;   mov si, 0           ; Initialize source index to point to the beginning of the array
-;print_loop:
-    ;mov ah, 02h         ; DOS function for printing a character
-    ;mov bx, keys
-    ;add bx, si
-    ;mov dx, bx ; Load the current element of the array into dl
-   ; add dx, 30h         ; Convert the number to its ASCII representation
-   ; int 21h             ; Call DOS interrupt to print the character
+;fill keys array
 
-  ;  inc si              ; Move to the next element in the array
- ;   cmp si, keysLength ; Compare the current index with the length of the array
-;    jl print_loop       ; If si < arrayLength, continue looping
+
 
 
  mov ah, 09h
  mov dx, offset mes
 int 21h
-end:
-mov ah, 09h
- mov dx, offset mes
-int 21h
+ending:
+
 
 end main
